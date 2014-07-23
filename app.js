3891ae81c15d0ad69e7eb31d19182e329fc4c12e
dlsso <supertonesfan@hotmail.com>
2014-07-23 10:04:37 -0600

    Moved data to models folder and set up linking

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var products = require('./models/products')
+
 
 var app = express();
 app.set('view engine', 'jade');
@@ -7,28 +9,7 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
-var products = [
-	{
-		name: 'Dark Roast',
-		price: 1.92,
-		origin: 'Brazil'
-	},
-	{
-		name: 'Darker Roast',
-		price: 2.50,
-		origin: 'Vietnam'
-	},
-	{
-		name: 'Light Roast',
-		price: 1.2,
-		origin: 'Indonesia'
-	},
-	{
-		name: 'Lighter Roast',
-		price: 2.92,
-		origin: 'Colombia'
-	}
-]
+
 
 
 app.get('/', function(req, res) {
---
+++ models/products.js
@@ -0,0 +1,24 @@
+var products = [
+	{
+		name: 'Dark Roast',
+		price: 1.92,
+		origin: 'Brazil'
+	},
+	{
+		name: 'Darker Roast',
+		price: 2.50,
+		origin: 'Vietnam'
+	},
+	{
+		name: 'Light Roast',
+		price: 1.2,
+		origin: 'Indonesia'
+	},
+	{
+		name: 'Lighter Roast',
+		price: 2.92,
+		origin: 'Colombia'
+	}
+]
+
+module.exports = products

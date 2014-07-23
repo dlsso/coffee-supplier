bcf95a483435f0f58083cdba481495b3e64de15d
dlsso <supertonesfan@hotmail.com>
2014-07-23 09:37:15 -0600

    Added some products to app.js

--- app.js
+++ app.js
@@ -7,8 +7,34 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
+var products = [
+	{
+		name: 'Dark Roast',
+		price: 1.92,
+		origin: 'Brazil'
+	},
+	{
+		name: 'Darker Roast',
+		price: 2.50,
+		origin: 'Vietnam'
+	},
+	{
+		name: 'Light Roast',
+		price: 1.2,
+		origin: 'Indonesia'
+	},
+	{
+		name: 'Lighter Roast',
+		price: 2.92,
+		origin: 'Colombia'
+	}
+]
+
+
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('index', {
+		coffeeProducts: products
+	});
 });
 
 var server = app.listen(9473, function() {
